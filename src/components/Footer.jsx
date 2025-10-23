b9f4c9831e2cfa2fbb58ad69c6bc6cd79001da53
Wingolf11 <juanesquiroz11@gmail.com>
2025-10-23 17:34:23 +0200

    Fix(Footer.css): styling footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,16 @@
+import "../styles/Footer.css";
+
 export default function Footer() {
 	return (
-    <footer>
-        <nav>
-            <a href="">about</a>
-            <a href="">contact</a>
-            <a href="">faq</a>
-            <a href="">terms</a>
-            <a href="">privacy</a>
-        </nav>
-        <p>a Mark Zuckerberg production Thefacebook &copy; 2004</p>
-    </footer>
-    );
+		<footer>
+			<nav>
+				<a href="">about</a>
+				<a href="">contact</a>
+				<a href="">faq</a>
+				<a href="">terms</a>
+				<a href="">privacy</a>
+			</nav>
+			<p>a Mark Zuckerberg production Thefacebook &copy; 2004</p>
+		</footer>
+	);
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,27 +7,32 @@ export default function About() {
 	return (
 		<>
 			<Card title="About Thefacebook">
-				<h1 id="about">[About]</h1>
+				<p id="about">[About]</p>
 				<Card title="The Project">
 					<p className="plain-text">
 						Thefacebook is an online directory that connects people through
 						social networks at colleges and universities
-					</p>
+                    </p>
 				</Card>
 				<Card title="The People">
                     <table className="the-people-list">
+                        <thead>
                         <tr>
-                            <th><a href="">Mark Zukerberg</a></th>
+                            <th><a className="links-thepeople" href="">Mark Zukerberg</a></th>
                             <td>Founder, Master and Commander, Enemy of the State.</td>
                         </tr>
                         <tr>
-                            <th><a href="">Eduardo Saverin</a></th>
+                            <th><a className="links-thepeople" href="">Eduardo Saverin</a></th>
                             <td>Business Stuff, Corporate Stuff, Brazilian Affairs.</td>
                         </tr>
                         <tr>
-                            <th><a href="">Andrew McCollum</a></th>
+                            <th><a className="links-thepeople" href="">Andrew McCollum</a></th>
                             <td>Graphic Art, General Rockstar.</td>
                         </tr>
+                        <tr>
+                            <th><a className="links-thepeople" href="">Contact Us</a></th>
+                        </tr>
+                        </thead>
                     </table>
                 </Card>
 				<Button></Button>
